5196008e2e918074ac58d52bf7c68bbfd9a22d35
Zihadul Islam <ahmedxihad@gmail.com>
2020-11-19 02:15:13 +0600

    create a card array with name and images

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const container = document.querySelector(".container");
+
+const cats = [
+  { name: "Chi", image: "images/cat1.jpg" },
+  { name: "Bobbi", image: "images/cat2.png" },
+  { name: "Jelly Bean", image: "images/cat3.jpeg" },
+  { name: "Dora", image: "images/cat4.png" },
+  { name: " Squeaky", image: "images/cat5.jpg" },
+  { name: " Wyatt", image: "images/cat6.jpg" },
+  { name: " Addison", image: "images/cat7.jpg" },
+  { name: " Lucy", image: "images/cat8.jpg" },
+  { name: " Niko", image: "images/cat9.jpg" },
+  { name: " Stella", image: "images/cat10.webp" },
+  { name: " Cara", image: "images/cat11.jpg" },
+  { name: " Rouge", image: "images/cat12.jpg" },
+  { name: " Ruby", image: "images/cat13.jpeg" },
+  { name: " Petyr", image: "images/cat14.jpg" },
+  { name: " Catelyn", image: "images/cat15.jpg" },
+];
