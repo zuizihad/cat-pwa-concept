cf26d9dcc2479b5b825d318f9e481ced1ae77409
Zihadul Islam <ahmedxihad@gmail.com>
2020-11-19 02:18:04 +0600

    loop through the array and show them in container

--- js/app.js
+++ js/app.js
@@ -17,3 +17,19 @@ const cats = [
   { name: " Petyr", image: "images/cat14.jpg" },
   { name: " Catelyn", image: "images/cat15.jpg" },
 ];
+
+const showCats = () => {
+  let output = "";
+  cats.forEach(
+    ({ name, image }) =>
+      (output += `
+        <div class="card">
+        <img class="card--avatar" src=${image} />
+        <h1 class="card--title">${name}</h1>
+        <a class="card--link" href="#">Taste</a>
+        </div>
+        `)
+  );
+  container.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCats);
